7361aa16fa383994687ed668b25bdf89f6c49cb4
KL-debbie <141379707+KL-debbie@users.noreply.github.com>
2024-07-07 22:54:01 +0000

    랜덤 숫자 고르기

--- rnd_collect.py
+++ rnd_collect.py
@@ -1,16 +1,16 @@
 from random import randint
 
 print('Welcome')
-pc_choice = randint(1,100)
+pc_choice = randint(1, 100)
 
 playing = True
 
 while playing:
   choice = int(input("Choose Number(1-100): "))
   if choice == pc_choice:
-    print('Won!!')
+    print('You Won!!')
     playing = False
   elif choice > pc_choice:
-    print('Lower')
+    print('Select Lower')
   elif choice < pc_choice:
-    print('Higher')
+    print('Select Higher')
