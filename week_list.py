ef95aac798f88d7ef437374871380b701a7041e1
KL-debbie <141379707+KL-debbie@users.noreply.github.com>
2024-07-08 04:52:53 +0000

    List_week

---
+++ week_list.py
@@ -0,0 +1,12 @@
+d_of_week = ['mon','tue','wed','thur','fri']
+d_of_week.count('wed')
+d_of_week.append('sat')
+d_of_week.append('sun')
+print(d_of_week)
+
+name = 'lieol'
+
+print(name.capitalize())
+print(name.replace(name[1], "p"))
+
+print(d_of_week.count('tue'))
