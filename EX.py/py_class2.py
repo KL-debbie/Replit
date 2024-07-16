ec42b5350872e0670e7845fa6543ec789e5c25b0
KL-debbie <141379707+KL-debbie@users.noreply.github.com>
2024-07-16 00:43:08 +0000

    파이썬 실습 예제

--- EX.py/py_class2.py
+++ EX.py/py_class2.py
@@ -1,42 +1,66 @@
 import random
 
+num = random.randint(0, 99)
+print(num)
+print(str(num).zfill(5))
+#-----------------------------------------------
 class Account:
 
-  account_count = 0
-  
+  account_count = 0    # 계좌가 만들어질때마다 생성자 호출
+
   def __init__(self, name, balance):
+    self.deposit_count = 0 # 입금 횟수
+    self.deposit_log = []
+    self.withdraw_log = []
+    
     self.name = name
     self.balance = balance
     self.bank = "SC은행"
-    num1 = random.randint(0, 999)
-    num2 = random.randint(0, 99)
-    num3 = random.randint(0, 999999)
+    #계좌번호 생성
+    num1 = random.randint(0, 999)  # 3자리
+    num2 = random.randint(0, 99)  # 2자리
+    num3 = random.randint(0, 999999)  # 6자리
 
-    num1 = str(num1).zfill(3)      # 1 -> '1' -> '001'
-    num2 = str(num2).zfill(2)      # 1 -> '1' -> '01'
-    num3 = str(num3).zfill(6)      # 1 -> '1' -> '0000001'
+    num1 = str(num1).zfill(3)  # 1 -> '1' -> '001'
+    num2 = str(num2).zfill(2)  # 1 -> '1' -> '01'
+    num3 = str(num3).zfill(6)  # 1 -> '1' -> '0000001'
     self.account_number = num1 + '-' + num2 + '-' + num3  # 001-01-000001
-    
-    Account.account_count += 1
+
+    Account.account_count += 1    # 계좌가 만들어질때마다 생성되므로 1씩 증가
 
   def get_account_num(cls):
-    print(cls.account_count)      # Account.account_count
-  
-  def deposit(self, amount):      # 입금 메서드
-    if amount >= 1:
+    print(cls.account_count)      # Account.account_count 생성된 계좌의 갯수 출력
+
+  def deposit(self, amount):      # 입금 메서드, amount 입금액
+    if amount >= 1:    # 1원 이상 입금 가능
+      self.deposit_log.append(amount)    # 입금할때마다 리스트에 저장
       self.balance += amount
 
-  def withdraw(self, amount):
-    if self.balance > amount:
+      # 이자 지급
+      self.deposit_count += 1
+      if self.deposit_count % 5 == 0:    # 입급 횟수가 5회가 될 때
+        # 이자 지금
+        self.balance = self.balance * 1.01
+
+  def withdraw(self, amount):      # 출금 메서드
+    if self.balance > amount: 
+      self.withdraw_log.append(amount)    # 출금 매역
       self.balance -= amount
 
-  def display_info(self):
+  def display_info(self):        # 정보 출력
     print('bank :', self.bank)
     print('name :', self.name)
     print('Account :', self.account_number)
-    print('balance :', f"{self.balance:,}")
+    print('balance :', f"{self.balance:,}")    # 3자리마다 , 출력하기
 
-# 계좌 객체 갯수
+  def withdraw_history(self):    # 출금
+    for amount in self.withdraw_log:
+      print(amount)
+      
+  def deposit_history(self):    # 입금
+    for amount in self.deposit_log:
+      print(amount)
+# 계좌 객체 개수
 '''
 kim = Account("김민수", 100)
 print(Account.account_count)
@@ -50,6 +74,8 @@ print(kim.name)
 print(kim.balance)
 print(kim.bank)
 print(kim.account_number)
+print(Account.account_count)
+
 
 # 입금 출금
 kimm = Account('leee', 157)
@@ -58,6 +84,44 @@ print(kimm.balance)
 kimm.deposit(100)
 kimm.withdraw(90)
 print(kimm.balance)
+print(Account.account_count)
 
+# Account 정보 출력
 pyy = Account('py', 100000)
 pyy.display_info()
+
+# 이자 지급
+p = Account("py", 10000)
+p.deposit(100000)
+p.deposit(100000)
+p.deposit(100000)
+p.deposit(33300)
+p.deposit(33300)
+print(p.balance)
+
+# 인스턴스 리스트 저장
+data = []
+k = Account('KIM', 100000)
+l = Account('LEE', 1000000000000)
+p = Account('PARK', 1000000000)
+
+data.append(k)
+data.append(l)
+data.append(p)
+
+#  1000000만원 이상인 고객 정보 출력
+for c in data:
+  if c.balance >= 1000000:
+    c.display_info()
+
+# 입금 출금 내역
+c = Account("CHO", 300)
+c.deposit(300)
+c.deposit(300)
+c.deposit(300)
+c.deposit(300)
+c.deposit_history()
+
+c.withdraw(400)
+c.withdraw(400)
+c.withdraw_history()
--- EX.py/py_class3.py
+++ EX.py/py_class3.py
@@ -1,5 +1,6 @@
-class car:    # class 정의
-  def __init__(self,wheel,price):
+class car:  # class 정의
+
+  def __init__(self, wheel, price):
     self.wheel = wheel
     self.price = price
 
@@ -7,26 +8,33 @@ class car:    # class 정의
     print('wheel :', self.wheel)
     print('price :', self.price)
 
-class my_car(car):        # car 클래스 상속
-  def __init__(self,wheel,price, move):
+
+class my_car(car):  # car 클래스 상속, class 클래스명(상속받을 클래스명)
+
+  def __init__(self, wheel, price, move):
     self.wheel = wheel
     self.price = price
     self.move = move
 
-class my_bike(car):      # car 클래스 상속
+
+class my_bike(car):  # car 클래스 상속
+
   def __init__(self, wheel, price, move):
     super().__init__(wheel, price)
+    # car.__init__(self, wheel, price)
     self.move = move
 
-  def info(self):        # 구동계 정보 출력
+  def info(self):  # 구동계 정보 출력
     super().info()
     print('move :', self.move)
-    
-#-----------------------------------------------    
+
+
+#-----------------------------------------------
+# 객체 생성 / self 자리에 저장하여 호출
 car1 = car(2, 1000)
 print(car1.wheel)
 print(car1.price)
-#-----------------------------------------------    
+#-----------------------------------------------
 car1 = car(2, 1000)
 car1.info()
 #-----------------------------------------------
@@ -34,5 +42,5 @@ car2 = my_car(2, 100, '자전거')
 print(car2.wheel)
 print(car2.move)
 #-----------------------------------------------
-bike = my_bike(2, 100,'시마노')
+bike = my_bike(2, 100, '시마노')
 bike.info()
